a3a4783083a8aca7d80574dcce92ea1c67c7f137
Diego M. Maldini Freyre <opensuse-tumbleweed@localhost.localdomain>
2020-11-11 14:23:04 -0300

    spinal-case

--- intermediate-algorithm-scripting/current.js
+++ intermediate-algorithm-scripting/current.js
@@ -1,56 +1,59 @@
-/**
- * Intermediate Algorithm Scripting: Spinal Tap Case
- * 
- * Convert a string to spinal case. Spinal case is 
- * all-lowercase-words-joined-by-dashes.
- */
-function spinalCase(str) {
-  const newStr = str.toLowerCase();
-  const newArr = newStr.split(" ");
-  return newArr.join("-");
+function styleHyphenFormat(string) {
+  function upperToHyphenLower(match, offset, string) {
+    return (offset > 0 ? '-' : '') + match.toLowerCase();
+  }
+  return string.replace(/[A-Z]/g, upperToHyphenLower);
 }
+
+console.log(styleHyphenFormat('ThisIsSpinalTap'))
 
 spinalCase('This Is Spinal Tap');
 
-// /**
-//  * 
-// /**
-//  * Intermediate Algorithm Scripting: Spinal Tap Case
-//  *
-//  * Convert a string to spinal case. Spinal case is
-//  * all-lowercase-words-joined-by-dashes.
-//  */
-// const replaceForSpacePlusLowerCase = (string, index) => {
-    
-// };
-// 
-// function spinalCase(str) {
-  // const newStr = str.trim();
-  // /**
-  //  * first, I'll search for upper case letters, and replace them for space + lower case
-  //  */
-  // const regex = /[A-Z]/;
-  // const index = str.search(regex);
-// 
-  // if (index >= 0) {
-    // const auxStr = replaceForSpacePlusLowerCase(str, index);
-// 
-  // }
-  // const newArr = newStr.split(" ");
-  // console.log(newArr.join("-"));
-  // return newArr.join("-");
-  // /**
-  //  * second, I'll split in spaces and join again with dashes
-  //  */
-// }
-// 
-// spinalCase("This Is Spinal Tap"); // should return "this-is-spinal-tap".
-// 
-// spinalCase("thisIsSpinalTap"); // should return "this-is-spinal-tap".
-// 
-// spinalCase("TheAndyGriffith_Show"); // should return "the-andy-griffith-show".
-// 
-// spinalCase("Teletubbies say Eh-oh"); // should return "teletubbies-say-eh-oh".
-// 
-// spinalCase("AllThe-small Things"); // should return "all-the-small-things".
-// 
+const insertLowerCasePlusSpace = (string, index) => {
+  const char = string[i];
+  const replacement = string[i] + " ";
+  string.replace(char, string[i])
+}
+
+const replaceUpperCasesInString = string => {
+  let newString = string.slice();
+  for (let i = 0; i < newString.length; i++) {
+    const char = newString[i];
+    const regex = /[A-Z]/;
+    if (regex.test(char)) insertLowerCasePlusSpace(newString, i);
+  }
+  /**
+   * inside a loop we call insertLowerCase
+   */
+  return newString;
+}
+
+function spinalCase(str) {
+  const newStr = str.trim();
+  /**
+   * first, I'll search for upper case letters, and replace them for space + lower case
+   */
+  
+  const index = str.search(regex);
+
+  if (index >= 0) {
+    const auxStr = replaceForSpacePlusLowerCase(str, index);
+
+  }
+  const newArr = newStr.split(" ");
+  console.log(newArr.join("-"));
+  return newArr.join("-");
+  /**
+   * second, I'll split in spaces and join again with dashes
+   */
+}
+
+spinalCase("This Is Spinal Tap"); // should return "this-is-spinal-tap".
+
+spinalCase("thisIsSpinalTap"); // should return "this-is-spinal-tap".
+
+spinalCase("TheAndyGriffith_Show"); // should return "the-andy-griffith-show".
+
+spinalCase("Teletubbies say Eh-oh"); // should return "teletubbies-say-eh-oh".
+
+spinalCase("AllThe-small Things"); // should return "all-the-small-things".
