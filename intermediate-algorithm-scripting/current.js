d7a56adfc80e13cd7ad01569e6dce74d1ed17f94
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-12 09:57:40 -0300

    current

--- intermediate-algorithm-scripting/current.js
+++ intermediate-algorithm-scripting/current.js
@@ -1,19 +1,17 @@
-const onlyAlphanumeric = (str) => {
-  const notAlphanumeric = /[^(\d|\w)]+/gi;
-  return str.replace(notAlphanumeric, "-");
+/**
+ * Intermediate Algorithm Scripting: Pig Latin
+ * 
+ * Pig Latin is a way of altering English Words. The rules are as follows:
+ * 
+ * - If a word begins with a consonant, take the first consonant or consonant 
+ * cluster, move it to the end of the word, and add "ay" to it.
+ * 
+ * - If a word begins with a vowel, just add "way" at the end.
+ * 
+ * Translate the provided string to Pig Latin. Input strings are guaranteed to be 
+ * English words in all lowercase.
+ */
+function translatePigLatin(str) {
+  
+  return str;
 }
-
-function styleHyphenFormat(propertyName) {
-  function upperToHyphenLower(match, offset, string) {
-    return (offset > 0 ? '-' : '') + match.toLowerCase();
-  }
-  return propertyName.replace(/[A-Z]/g, upperToHyphenLower);
-}
-
-function spinalCase(str) {
-  const firstStr = str.replaceAll("_", "-");
-  const newStr = styleHyphenFormat(firstStr);
-  return onlyAlphanumeric(newStr);
-}
-
-console.log(spinalCase("TheAndyGriffith_Show"))
