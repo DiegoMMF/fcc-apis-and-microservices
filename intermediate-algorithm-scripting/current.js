e6dc03f0de2e09c1d501b62813048117d0652ca7
DiegoMMF <ubuntu@groovy.gorilla>
2020-11-17 20:30:26 -0300

    sorted-union

--- intermediate-algorithm-scripting/current.js
+++ intermediate-algorithm-scripting/current.js
@@ -1,34 +1,11 @@
 /**
- * Intermediate Algorithm Scripting: Missing letters
- * Find the missing letter in the passed letter range and return it.
- * If all letters are present in the range, return undefined.
+ * Intermediate Algorithm Scripting: Convert HTML Entities
+ * 
+ * Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a 
+ * string to their corresponding HTML entities.
  */
-
-const areConsecutives = (a, b) => {
-  return (a.codePointAt(0) + 1 === b.codePointAt(0)) ? true : false;
-}
-
-function fearNotLetter(str) {
-  const firstChar = str[0];
-  const lastChar = str[str.length - 1];
-  if (str.length === 1 || areConsecutives(firstChar, lastChar)) {
-    return undefined;
-  };
-  return (areConsecutives(firstChar, str[1])) 
-    ? fearNotLetter(str.slice(1))
-    : String.fromCodePoint(str.codePointAt(0)+1);
-  
-}
-
-console.log(fearNotLetter("ac"));
-
-/*
-  const a = "\u0041"
-  console.log(str.charCodeAt(0));
-  console.log(str.charCodeAt(1));
-  console.log(str.charCodeAt(2));
-  console.log(str.charCodeAt(3));
-  console.log(str.charCodeAt(4));
-  console.log(a.normalize());
+function convertHTML(str) {
   return str;
-*/
+}
+
+convertHTML("Dolce & Gabbana");
