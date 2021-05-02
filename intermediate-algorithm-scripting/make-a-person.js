1e3a09ad4310c1c4d45ebe19e568b3a9d9f672d0
DiegoMMF <diegomaldinifreyre@gmail.com>
2021-05-02 15:24:39 -0300

    make-person DONE

--- intermediate-algorithm-scripting/make-a-person.js
+++ intermediate-algorithm-scripting/make-a-person.js
@@ -1,27 +1,18 @@
 /**
  * Make a Person
  * Fill in the object constructor with the following methods below:
- * getFirstName()
- * 
- * getLastName()
- * 
+ * getFirstName() 
+ * getLastName() 
  * getFullName()
- * 
  * setFirstName(first)
- * 
  * setLastName(last)
- * 
  * setFullName(firstAndLast)
- * 
  * Run the tests to see the expected output for each method. The
  * methods that take an argument must accept only one argument and
  * it has to be a string. These methods must be the only available
  * means of interacting with the object.
- * 
  */
  const Person = function(firstAndLast) {
-  // Only change code below this line
-  // Complete the method below and implement the others similarly
   let [firstName, lastName] = firstAndLast.split(" ");
   this.getFirstName = function() {
     return firstName;
